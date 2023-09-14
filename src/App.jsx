402faba47f3adf996b02d76c9485ffb49bcad744
Atik Sahariyar <atiksahariyar88@gmail.com>
2023-09-14 18:59:27 +0600

    Installation complete react App & Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-semibold text-center my-8 '>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
